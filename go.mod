3e2f1415894e1cc6b8cbcae72710cb75fad8aff5
Павлов Иван Иванович <w3dipmail@gmail.com>
2021-02-07 15:54:07 +0300

    before any success

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
 module hw7_microservice
 
-go 1.14
+go 1.15
 
 require (
 	github.com/go-test/deep v1.0.6 // indirect
-	github.com/golang/protobuf v1.4.0 // indirect
-	google.golang.org/grpc v1.29.1
+	github.com/golang/protobuf v1.4.1
+	google.golang.org/grpc v1.32.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.25.0
 )
--- service.go
+++ service.go
@@ -1,4 +1,57 @@
 package main
 
-// тут вы пишете код
-// обращаю ваше внимание - в этом задании запрещены глобальные переменные
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"sync"
+)
+
+type BizServerImpl struct {
+	mu  sync.RWMutex
+	ctx context.Context
+	acl string
+}
+
+func NewBizManager(ctx context.Context, acl string) *BizServerImpl {
+	return &BizServerImpl{
+		mu:  sync.RWMutex{},
+		ctx: ctx,
+		acl: acl,
+	}
+}
+
+func (server *BizServerImpl) Check(ctx context.Context, nothing *Nothing) (*Nothing, error) {
+	return nil, nil
+}
+
+func (server *BizServerImpl) Add(ctx context.Context, nothing *Nothing) (*Nothing, error) {
+	return nil, nil
+}
+
+func (server *BizServerImpl) Test(ctx context.Context, nothing *Nothing) (*Nothing, error) {
+	return nil, nil
+}
+
+func (server *BizServerImpl) mustEmbedUnimplementedBizServer() {
+}
+
+func StartMyMicroservice(ctx context.Context, addr string, acl string) error {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalln("cant listet port", err)
+	}
+
+	server := grpc.NewServer()
+
+	RegisterBizServer(server, NewBizManager(ctx, acl))
+
+	fmt.Printf("starting server at %s", addr)
+	err = server.Serve(lis)
+	if err != nil {
+		return err
+	}
+	return nil
+}
