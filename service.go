c932cf2dd491565f6c80aa4e745f4daa174cc3da
Павлов Иван Иванович <w3dipmail@gmail.com>
2021-02-07 18:35:01 +0300

    maybe it's work - server

--- service.go
+++ service.go
@@ -39,19 +39,69 @@ func (server *BizServerImpl) mustEmbedUnimplementedBizServer() {
 }
 
 func StartMyMicroservice(ctx context.Context, addr string, acl string) error {
-	lis, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalln("cant listet port", err)
-	}
 
-	server := grpc.NewServer()
+	//wg := &sync.WaitGroup{}
+	//wg.Add(1)
+	fmt.Println("Run server")
+	go func( /*wg *sync.WaitGroup, */ ctx context.Context, addr string /*server *grpc.Server, lis net.Listener*/) {
+		lis, err := net.Listen("tcp", addr)
+		if err != nil {
+			log.Fatalln("cant listet port", err)
+		}
 
-	RegisterBizServer(server, NewBizManager(ctx, acl))
+		server := grpc.NewServer()
 
-	fmt.Printf("starting server at %s", addr)
-	err = server.Serve(lis)
-	if err != nil {
-		return err
-	}
+		RegisterBizServer(server, NewBizManager(ctx, acl))
+		//defer wg.Done()
+
+		//fmt.Printf("starting server at %s", addr)
+		//fmt.Printf("Try to start")
+		go server.Serve(lis)
+		//if err != nil {
+		//	log.Fatalln("cant start server", err)
+		//}
+		fmt.Println("Server is running")
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("finish called")
+				server.Stop()
+				//time.Sleep(time.Duration(2) * 10 * time.Millisecond)
+				//lis.Close()
+				return
+				//default:
+				//	fmt.Printf("noop")
+			}
+		}
+
+	}( /*wg, */ ctx /*server, lis*/, addr)
+
+	//time.Sleep(time.Duration(2) * 1000 * time.Millisecond)
+
+	//select {
+	//		case <-ctx.Done():
+	//			fmt.Printf("finish called")
+	//			server. GracefulStop()
+	//			lis.Close()
+	//			return nil
+	//			//default:
+	//		}
+	//<-ctx.Done()
+
+	//server.Stop()
+	//lis.Close()
+
+	//for {
+	//	select {
+	//	case <-ctx.Done():
+	//		fmt.Printf("finish called")
+	//		server. /*Graceful*/ Stop()
+	//		return nil
+	//		//default:
+	//	}
+	//}
+	//fmt.Printf("Waiting for ending...")
+	fmt.Println("Waiting for termination")
+	//wg.Wait()
 	return nil
 }
